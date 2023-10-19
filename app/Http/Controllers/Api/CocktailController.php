8f0125e35a403780eb7819811c1fa101b8fe9ae6
MicheleGresta <malachia_1997@hotmail.it>
2023-10-19 17:18:16 +0200

    controller, api route added

--- app/Http/Controllers/Api/CocktailController.php
+++ app/Http/Controllers/Api/CocktailController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Cocktail;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,9 @@ class CocktailController extends Controller
      */
     public function index()
     {
-        //
+        $cocktails = Cocktail::all();
+
+        return response()->json($cocktails);
     }
 
     /**
