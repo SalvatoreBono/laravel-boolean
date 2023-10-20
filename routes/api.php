0d844d2ead0cd78ee79d4fe88a468324e7b6e36a
Salvatore Bono <salvatorebono2001@gmail.com>
2023-10-20 15:36:11 +0200

    add Contact migration, controller, form request.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CocktailController;
+use App\Http\Controllers\Api\ContactController;
 use App\Models\Cocktail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('cocktails', [CocktailController::class, "index"]);
+Route::post('contact', [ContactController::class, "store"]);
